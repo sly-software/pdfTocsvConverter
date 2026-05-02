0ff6943d93dee5a22a1598da601dd24f8f25b794
Silvester Charles <silvestaisasi@gmail.com>
2026-05-03 02:13:46 +0300

    Added the final processing .sh script

---
+++ scripts/extractQTdata.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/bash
+#!/usr/bin/env python3
+
+# you need docling to run this code
+# best to do is to create a python environment and install docling
+# via pip then activate it and run this script follow steps below
+#**************************************
+# python venv -m doclin
+# source .docling/bin/activate
+# pip install docling
+#**************************************
+
+
+filePath=$(realpath $1)
+fileToProces=$(basename $filePath .pdf)
+
+# It will create samples.tables.csv file for further maniptaion
+python3 pdfToMd.py $filePath | grep "^|" | python3 get.tables.py
+
+# Remove VAT and duplicates header colums and save to ${fileToProces}.csv
+cat samples.tables.csv | \
+grep "VAT" | \
+head -n1 > ../output/${fileToProces}.csv && cat samples.tables.csv | \
+grep -v "VAT" >> ../output/${fileToProces}.csv
+
+
+## Error detection
+if [ $? -eq 0 ]; then
+    echo "PDF Extracted successfully!"
+
+    # cleanup Intermediate files$
+    rm samples.tables.csv
+else
+    echo "Error: The python script crashed. Did you activate the correct .env"
+    exit 1
+fi
